bb6937254c5c4f93a76f2da525437e3a917f059b
Javier Quiroga <javier.quiroga.e@gmail.com>
2023-01-02 17:18:26 -0500

    done

--- serial.py
+++ serial.py
@@ -19,4 +19,23 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        '''create instance with start value'''
+        self.start = start
+        self.gen = start - 1
+    
+    def __repr__(self):
+        """Show representation."""
+
+        return f"<SerialGenerator start={self.start} next={self.gen + 1}>"
+
+        
+    def generate(self):
+        """Return next serial."""
+        self.gen += 1
+        return self.gen
+
+    def reset(self):
+        """Reset number to original start."""
+        self.gen = self.start - 1
 
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,14 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    def __init__(self, path):
+        '''creates alist of words from a text file found using path'''
+        file = open(path)
+        self.list_of_words = [line.strip() for line in file]
+        file.close()
+        print(f"{len(self.list_of_words)} words read")
+
+    def random(self):
+        """Return random word."""
+        return random.choice(self.list_of_words)
